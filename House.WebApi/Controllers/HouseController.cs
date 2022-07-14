99e325892b9aa1fde431687f33f425f883ba78e1
Marcin <mwasyl@gmail.com>
2022-07-14 10:34:22 +0200

    new controller

---
+++ House.WebApi/Controllers/HouseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace House.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HouseController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Api dziala");
+        }
+    }
+}
