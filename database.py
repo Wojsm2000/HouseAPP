b7b625a3c74427299a12f135e378daf7170a8618
Wojsm2000 <95697097+Wojsm2000@users.noreply.github.com>
2022-09-02 01:53:46 +0200

    Add files via upload
    Added database and csv file just in case. Reworked both , database and main files

--- database.py
+++ database.py
@@ -2,47 +2,46 @@ from main import data
 import sqlite3 as sl
 import pandas
 import winsound
-
+import csv
+# con = sl.connect('my-test.db')
+#
+# def deleteSqliteRecord(city):
+#     try:
+#
+#         cursor = con.cursor()
+#         print("Connected to SQLite")
+#
+#         sql_update_query = """DELETE from USER where city = ?"""
+#         cursor.execute(sql_update_query, (city,))
+#         con.commit()
+#         print("Record deleted successfully")
+#
+#         cursor.close()
+#
+#     except sqlite3.Error as error:
+#         print("Failed to delete reocord from a sqlite table", error)
+#     finally:
+#         if con:
+#             con.close()
+#             print("sqlite connection is closed")
+#
+#
+# con = sl.connect('my-test.db')
+#
+# deleteSqliteRecord("Łódź")
 con = sl.connect('my-test.db')
 
-def deleteSqliteRecord(city):
-    try:
 
-        cursor = con.cursor()
-        print("Connected to SQLite")
-
-        sql_update_query = """DELETE from USER where city = ?"""
-        cursor.execute(sql_update_query, (city,))
-        con.commit()
-        print("Record deleted successfully")
-
-        cursor.close()
-
-    except sqlite3.Error as error:
-        print("Failed to delete reocord from a sqlite table", error)
-    finally:
-        if con:
-            con.close()
-            print("sqlite connection is closed")
-
-
-con = sl.connect('my-test.db')
-
-deleteSqliteRecord("Łódź")
-
-
-
-# with con:
-#     con.execute("""
-#     CREATE TABLE IF NOT EXISTS  USER (
-#     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-#     link TEXT
-#     price DOUBLE,
-#     area DOUBLE,
-#     city TEXT,
-#     district TEXT
-#     );
-#     """)
+with con:
+    con.execute("""
+    CREATE TABLE IF NOT EXISTS  USER (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    price DOUBLE,
+    area DOUBLE,
+    city TEXT,
+    district TEXT
+    );
+    """)
 
 city,dist,price,area,link=data()
 
@@ -55,10 +54,14 @@ df_user=pandas.DataFrame({
 
 
 })
-winsound.Beep(500,5000)
-print(df_user)
-df_user.to_sql('USER', con)
 
+
+winsound.Beep(500,5000)
+# print(df_user)
+
+
+df_user.to_sql('USER', con,if_exists='replace')
+df_user.to_csv('data.csv')
 with con:
     data = con.execute("SELECT * FROM USER ")
     for row in data:
